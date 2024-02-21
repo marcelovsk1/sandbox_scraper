4dc64f5f6408ac088773dfdb8e4a8250e942dd71
Marcelo Amaral Alves <marceloaa@live.com>
2024-02-21 17:17:32 -0300

    starttime endtime facebook

--- Scraper/sandbox/facebook.py
+++ Scraper/sandbox/facebook.py
@@ -1,21 +1,16 @@
 import json
+import re
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
-from fuzzywuzzy import fuzz
 from datetime import datetime
 import requests
 from geopy.geocoders import Nominatim
-import webbrowser
 
 def scroll_to_bottom(driver, max_scroll=5):
     for _ in range(max_scroll):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)  # Esperar um tempo para a página carregar completamente
-
-def calculate_similarity(str1, str2):
-    return fuzz.token_sort_ratio(str1, str2)
 
 def format_date(date_str, source):
     if date_str is None:
@@ -32,46 +27,6 @@ def format_date(date_str, source):
         # Eventbrite: Sunday, March 3
         formatted_date = datetime.strptime(date_str, '%A, %B %d')
         return formatted_date
-    else:
-        return None
-
-def format_location(location_str, source):
-    if source == 'Facebook':
-        # If location contains a comma, we split into location name and address
-        if ',' in location_str:
-            location, address = location_str.split(',', 1)
-            return {
-                'Location': location.strip(),
-                'Address': address.strip()
-            }
-        else:
-            # If there is no comma, we assume it is just the location name
-            return {
-                'Location': location_str.strip(),
-            }
-    elif source == 'Eventbrite':
-        # Eventbrite already provides separate location and address
-        return {
-            'Location': location_str.strip(),
-        }
-    elif source == 'Google':
-        # Use Google Places API to get formatted location and additional information
-        api_key = 'AIzaSyD4K3294QGT9YUSquGZ_G82YMI856E0BzA'
-        url = f'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={location_str}&inputtype=textquery&fields=formatted_address,geometry&key={api_key}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            if data['status'] == 'OK' and len(data['candidates']) > 0:
-                formatted_address = data['candidates'][0]['formatted_address']
-                location = formatted_address.split(',')[0]  # Extracting the location name
-                return {
-                    'Location': location.strip(),
-                    'FormattedAddress': formatted_address,
-                    'Latitude': data['candidates'][0]['geometry']['location']['lat'],
-                    'Longitude': data['candidates'][0]['geometry']['location']['lng']
-                }
-        # If unable to fetch data from Google Places API, return None
-        return None
     else:
         return None
 
@@ -117,7 +72,9 @@ def scrape_facebook_events(driver, url, selectors, max_scroll=5):
         event_title_elem = event_page.find('span', class_='x1lliihq x6ikm8r x10wlt62 x1n2onr6')
         if event_title_elem:
             event_title = event_title_elem.text.strip()
-            if any(calculate_similarity(event_title, existing_title) >= 90 for existing_title in unique_event_titles):
+            # Remova a chamada para a função calculate_similarity
+            # if any(calculate_similarity(event_title, existing_title) >= 90 for existing_title in unique_event_titles):
+            if any(event_title == existing_title for existing_title in unique_event_titles):
                 continue
         else:
             continue  # Skip this event if title element is not found
@@ -132,10 +89,20 @@ def scrape_facebook_events(driver, url, selectors, max_scroll=5):
         # Abre o Google Maps com a localização do evento
         google_maps_url = open_google_maps(latitude, longitude)
 
+        date_text = event_page.find('div', class_='x1e56ztr x1xmf6yo').text.strip() if event_page.find('div', class_='x1e56ztr x1xmf6yo') else None
+
+        # Extrair StartTime e EndTime usando regex
+        match = re.search(r'(\d{1,2}:\d{2}\s?[AP]M)\s?–\s?(\d{1,2}:\d{2}\s?[AP]M)', date_text)
+        if match:
+            start_time, end_time = match.groups()
+        else:
+            # Se não houver correspondência, definir StartTime como None e EndTime como None
+            start_time, end_time = None, None
+
         event_info = {
             'Title': event_title,
             'Description': event_page.find('div', class_='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs').text.strip() if event_page.find('div', class_='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs') else None,
-            'Date': event_page.find('div', class_='x1e56ztr x1xmf6yo').text.strip() if event_page.find('div', class_='x1e56ztr x1xmf6yo') else None,
+            'Date': date_text,
             'Location': location_text,
             'Latitude': latitude,
             'Longitude': longitude,
@@ -143,7 +110,9 @@ def scrape_facebook_events(driver, url, selectors, max_scroll=5):
             'ImageURL': event_page.find('img', class_='xz74otr x1ey2m1c x9f619 xds687c x5yr21d x10l6tqk x17qophe x13vifvy xh8yej3')['src'] if event_page.find('img', class_='xz74otr x1ey2m1c x9f619 xds687c x5yr21d x10l6tqk x17qophe x13vifvy xh8yej3') else None,
             'Organizer': event_page.find('span', class_='xt0psk2').text.strip() if event_page.find('span', class_='xt0psk2') else None,
             'Organizer_IMG': event_page.find('img', class_='xz74otr')['src'] if event_page.find('img', class_='xz74otr') else None,
-            'EventUrl': event_url
+            'EventUrl': event_url,
+            'StartTime': start_time,
+            'EndTime': end_time
         }
 
         all_events.append(event_info)
@@ -153,7 +122,7 @@ def scrape_facebook_events(driver, url, selectors, max_scroll=5):
 
     return all_events
 
-def main():
+if __name__ == "__main__":
     sources = [
         {
             'name': 'Facebook',
@@ -183,6 +152,3 @@ def main():
         json.dump(all_events, f, indent=4)
 
     driver.quit()
-
-if __name__ == "__main__":
-    main()
